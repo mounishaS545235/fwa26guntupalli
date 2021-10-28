98c4b05fef9499b2a6b62b22b5f583a17e2ebe61
mounishaS545235 <89499369+mounishaS545235@users.noreply.github.com>
2021-10-27 22:58:46 -0500

    Added computation

---
+++ routes/computation.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var router = express.Router();
+var rno;
+var acosh;
+var log1p;
+var tan;
+var sqrt;
+
+/* GET users listing. */
+
+router.get("/", function (req, res, next) {
+  random = Math.random() * (100 - 1) + 1;
+
+  acosh = Math.acosh(random);
+
+  log1p = Math.log1p(random);
+
+  tan = Math.tan(random);
+
+  sqrt = Math.sqrt(random);
+
+  res.send(`The value of Math.acosh() is ${acosh} <br>
+
+            The value of Math.log1p() is ${log1p} <br>
+
+            The value of Math.tan() is ${tan} <br>
+            
+            The value of Math.sqrt() is (${sqrt})`);
+});
+
+module.exports = router;
