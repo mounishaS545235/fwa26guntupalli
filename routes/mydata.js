6b68b1c72ceb32b0cff8a5e9fc0bc2ab2f4634df
mounishaS545235 <89499369+mounishaS545235@users.noreply.github.com>
2021-10-27 20:35:57 -0500

    new endpoint

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,8 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'Mounisha Guntupalli' });
-  res.render('mydata', { fact : 'I love learning Turkish language by watching turkey series.'});
+  res.render('mydata', { 
+    title: 'Mounisha Guntupalli',
+    fact: 'I love learning Turkish language by watching turkey series.'
+  });
 });
 
 module.exports = router;
